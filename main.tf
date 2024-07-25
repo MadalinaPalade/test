21d31df80e6dc0d69f057874df98e000a11681ab
MadalinaPalade <palade.madalina@gmail.com>
2024-07-25 12:07:45 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
 
+terraform {
+#       backend "test" {
+#         # The name of your Terraform Cloud organization.
+#         organization = "MadalinaPaladeOrganization"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "MadalinaPaladeWorkspace"
+#         }
+#       }
+#     }
